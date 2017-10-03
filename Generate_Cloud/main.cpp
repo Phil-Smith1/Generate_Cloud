35fdf832c03c05d582024ea56e6d1ccefddfc08d
Phil Smith <philsmith@Phils-MacBook-Pro.local>
2017-10-03 14:10:38 +0100

    Initial Commit

---
+++ Generate_Cloud/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Generate_Cloud
+//
+//  Created by Phil Smith on 03/10/2017.
+//  Copyright © 2017 Phil Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
